bb4dddcb4935299fe6db82912f393aa427961e40
haotianlong <haotianlong@tencent.com>
2023-12-27 15:45:41 +0800

    feat: support miniprogram build

--- tsup.config.miniprogram.ts
+++ tsup.config.miniprogram.ts
@@ -3,15 +3,20 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['./src/index.ts'],
   clean: true,
-  outDir: 'dist',
+  outDir: 'miniprogram_dist',
   dts: true,
   // we need to keep minify false, since webpack magic comments
   // will be stripped if minify.
   minify: false,
-  format: ['esm', 'cjs'],
-  target: 'esnext',
+  format: ['cjs'],
+  target: 'es5',
+  noExternal: ['@noble/curves'],
   tsconfig: 'tsconfig.json',
   esbuildOptions(options) {
     options.define.__BUILD_TS__ = Date.now().toString();
+    options.define.import = 'require';
+    options.supported = {
+      'dynamic-import': false,
+    }
   }
 });
